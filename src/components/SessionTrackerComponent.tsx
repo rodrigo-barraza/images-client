19c45120728209d135f1fdaca534927171550a50
Rodrigo Barraza <hello@rod.dev>
2026-07-20 18:52:32 -0700

    feat: update dependency versions and enable heatmap and replay in SessionTrackerComponent

--- src/components/SessionTrackerComponent.tsx
+++ src/components/SessionTrackerComponent.tsx
@@ -4,6 +4,6 @@ import { SessionTrackerComponent as LibrarySessionTracker } from "@rodrigo-barra
 import { PROJECT_NAME } from "@/config";
 
 export default function SessionTrackerComponent() {
-  return <LibrarySessionTracker projectId={PROJECT_NAME} />;
+  return <LibrarySessionTracker projectId={PROJECT_NAME} replay heatmap />;
 }
 
